06be6a7c6bb013f787ee61d088e3cfe9ae2e2195
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-18 22:23:13 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,23 @@
+mod gameplay;
+mod level_select;
+mod main_menu;
+
 use bevy::prelude::*;
 use bevy_obj::ObjPlugin;
 
-fn main() {
-	App::new()
-		.add_plugins((DefaultPlugins, ObjPlugin))
-		.add_systems(Startup, setup)
-		.run();
+#[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
+enum MainState {
+	#[default]
+	MainMenu,
+	LevelSelect,
+	Gameplay,
 }
 
-/// set up a simple 3D scene
-fn setup(
-	mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>,
-	asset_server: Res<AssetServer>,
-) {
-	// circular base
-	commands.spawn(PbrBundle {
-		mesh: meshes.add(Circle::new(4.0)),
-		material: materials.add(Color::WHITE),
-		transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-		..default()
-	});
-	// obj
-	commands.spawn(PbrBundle {
-		mesh: asset_server.load("42.obj"),
-		material: materials.add(Color::srgb_u8(124, 144, 255)),
-		transform: Transform::from_xyz(0.0, 0.5, 0.0),
-		..default()
-	});
-	// light
-	commands.spawn(PointLightBundle {
-		point_light: PointLight {
-			shadows_enabled: true,
-			..default()
-		},
-		transform: Transform::from_xyz(4.0, 8.0, 4.0),
-		..default()
-	});
-	// camera
-	commands.spawn(Camera3dBundle {
-		transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-		..default()
-	});
+fn main() {
+	App::new()
+		.init_state::<MainState>()
+		.add_plugins((DefaultPlugins, ObjPlugin))
+		.add_systems(OnEnter(MainState::MainMenu), main_menu::setup)
+		.add_systems(OnEnter(MainState::Gameplay), gameplay::setup)
+		.run();
 }
---
+++ src/main_menu/mod.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+pub fn setup(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+	asset_server: Res<AssetServer>,
+) {
+}
