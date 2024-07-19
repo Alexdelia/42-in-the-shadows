e955a42dcc58f62917c0e7d2e1d6a38bbfb41e8f
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-19 21:59:44 +0200

    organize + `GameplayPlugin`

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -1,37 +1,24 @@
+mod scene;
+
 use bevy::prelude::*;
 
-pub fn setup(
-	mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>,
-	asset_server: Res<AssetServer>,
-) {
-	// circular base
-	commands.spawn(PbrBundle {
-		mesh: meshes.add(Circle::new(4.0)),
-		material: materials.add(Color::WHITE),
-		transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-		..default()
-	});
-	// obj
-	commands.spawn(PbrBundle {
-		mesh: asset_server.load("42.obj"),
-		material: materials.add(Color::srgb_u8(124, 144, 255)),
-		transform: Transform::from_xyz(0.0, 0.5, 0.0),
-		..default()
-	});
-	// light
-	commands.spawn(PointLightBundle {
-		point_light: PointLight {
-			shadows_enabled: true,
-			..default()
-		},
-		transform: Transform::from_xyz(4.0, 8.0, 4.0),
-		..default()
-	});
-	// camera
-	commands.spawn(Camera3dBundle {
-		transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-		..default()
-	});
+use crate::{despawn, MainState};
+
+pub struct GameplayPlugin;
+
+impl Plugin for GameplayPlugin {
+	fn build(&self, app: &mut App) {
+		app.add_systems(OnEnter(MainState::Gameplay), scene::spawn)
+			.add_systems(OnExit(MainState::Gameplay), despawn::<scene::GameplayScene>)
+			.add_systems(Update, key_handler);
+	}
+}
+
+fn key_handler(keys: Res<ButtonInput<KeyCode>>, mut next_main_state: ResMut<NextState<MainState>>) {
+	for key in keys.get_just_pressed() {
+		match key {
+			KeyCode::Escape => next_main_state.set(MainState::MainMenu),
+			_ => {}
+		}
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,20 @@ use bevy_obj::ObjPlugin;
 
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 enum MainState {
-	#[default]
 	MainMenu,
 	LevelSelect,
+	#[default]
 	Gameplay,
 }
 
 fn main() {
 	App::new()
-		.add_plugins((DefaultPlugins, ObjPlugin, main_menu::MainMenuPlugin))
+		.add_plugins((
+			DefaultPlugins,
+			ObjPlugin,
+			main_menu::MainMenuPlugin,
+			gameplay::GameplayPlugin,
+		))
 		.init_state::<MainState>()
-		.add_systems(OnEnter(MainState::Gameplay), gameplay::setup)
 		.run();
 }
