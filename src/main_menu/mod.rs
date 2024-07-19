8f466da7bf0dd47c8de459342d35fe0c9c4f8516
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-19 21:46:56 +0200

    switch to gameplay

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -9,6 +9,16 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
 	fn build(&self, app: &mut App) {
 		app.add_systems(OnEnter(MainState::MainMenu), scene::spawn)
-			.add_systems(OnExit(MainState::MainMenu), despawn::<scene::MainMenuScene>);
+			.add_systems(OnExit(MainState::MainMenu), despawn::<scene::MainMenuScene>)
+			.add_systems(Update, key_handler);
+	}
+}
+
+fn key_handler(keys: Res<ButtonInput<KeyCode>>, mut next_main_state: ResMut<NextState<MainState>>) {
+	for key in keys.get_just_pressed() {
+		match key {
+			KeyCode::Enter => next_main_state.set(MainState::Gameplay),
+			_ => {}
+		}
 	}
 }
